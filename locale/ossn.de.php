303db3c373150935cbf81fbc0c6b385d55afacf1
Arsalan Shah <manactric@gmail.com>
2026-02-07 20:00:23 +0500

    Create ossn.de.php

---
+++ locale/ossn.de.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Open Source Social Network
+ *
+ * @package   Open Source Social Network
+ * @author    Core Team
+ * @copyright (C) OpenTeknik LLC
+ * @license   Open Source Social Network License (OSSN LICENSE)  http://www.opensource-socialnetwork.org/licence
+ * @link      https://www.opensource-socialnetwork.org/
+ */
+ossn_register_languages('de', array(
+    'ossngadget:user:friends' => 'Freunde des Nutzers',
+    'ossngadget:user:albums' => 'Alben des Nutzers',
+    'ossngadget:available' => 'Verfügbar',
+    'ossngadget:guide' => 'Sie können die Gadgets per Drag & Drop verschieben. Es kann jeweils nur ein Gadget verwendet werden.',
+    'ossngadget:site:search' => 'Suche',
+    'ossngadget:user:online' => 'Mitglieder online',
+    'ossngadget:whoisonline:no' => 'Niemand ist online!',
+    'ossngadget:edit' => 'Layout bearbeiten',
+    'ossngadget:view:all' => 'Alle anzeigen',
+    'ossngadget:site:events' => 'Bevorstehende Veranstaltungen',
+    'ossngadget:site:marketplace' => 'Neueste Produkte',
+    'ossngadget:site:mp3' => 'Neueste MP3-Dateien',
+    'ossngadget:site:files' => 'Neueste Dateien',
+    'ossngadget:user:latest' => 'Neueste Mitglieder',
+));
